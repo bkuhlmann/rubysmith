2522cfcd32df79e436c94021f60ae63c1e1b0b92
Brooke Kuhlmann <brooke@alchemists.io>
2020-09-28 21:35:52 -0600

    Added version release notes

--- lib/rubysmith/identity.rb
+++ lib/rubysmith/identity.rb
@@ -4,7 +4,7 @@ module Rubysmith
   module Identity
     NAME = "rubysmith"
     LABEL = "Rubysmith"
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
     SUMMARY = "A command line interface for smithing Ruby projects."
   end
