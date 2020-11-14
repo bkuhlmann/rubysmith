19fd82ab761221df34568bc2d8828c35073888c1
Brooke Kuhlmann <brooke@alchemists.io>
2020-11-14 09:45:05 -0700

    Updated Rubocop gems
    Leverages the move to Rubocop's major version release in the 1.x.x
    series. This wasn't possible to do when Rubocop 1.0.0 was first
    released because Rubocop RSpec needed a 2.0.0 release. With both those
    releases out the door, it is now safe to upgrade to these gems.
    
    Details:
    
    - [Rubocop](https://chl.li/daBAS).
    - [Rubocop RSpec](https://chl.li/hKS0m).

--- spec/lib/rubysmith/builders/bundler_spec.rb
+++ spec/lib/rubysmith/builders/bundler_spec.rb
@@ -191,10 +191,10 @@ RSpec.describe Rubysmith::Builders::Bundler, :realm do
 
           group :development do
             gem "rake", "~> 13.0"
-            gem "rubocop", "~> 0.92"
+            gem "rubocop", "~> 1.3"
             gem "rubocop-performance", "~> 1.8"
             gem "rubocop-rake", "~> 0.5"
-            gem "rubocop-rspec", "~> 1.43"
+            gem "rubocop-rspec", "~> 2.0"
           end
         CONTENT
       end
@@ -254,10 +254,10 @@ RSpec.describe Rubysmith::Builders::Bundler, :realm do
             gem "rake", "~> 13.0"
             gem "reek", "~> 6.0"
             gem "rspec", "~> 3.9"
-            gem "rubocop", "~> 0.92"
+            gem "rubocop", "~> 1.3"
             gem "rubocop-performance", "~> 1.8"
             gem "rubocop-rake", "~> 0.5"
-            gem "rubocop-rspec", "~> 1.43"
+            gem "rubocop-rspec", "~> 2.0"
             gem "simplecov", "~> 0.19"
           end
         CONTENT
