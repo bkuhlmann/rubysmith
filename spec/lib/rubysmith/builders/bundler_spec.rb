fee52a906ea2777f0a2aba745ba13b00f96bf233
Brooke Kuhlmann <brooke@alchemists.io>
2020-12-29 11:10:21 -0700

    Updated to Refinements 8.0.0
    Adds Ruby 3.0.0 support.
    
    [Details](https://www.alchemists.io/projects/refinements/changes.html).

--- spec/lib/rubysmith/builders/bundler_spec.rb
+++ spec/lib/rubysmith/builders/bundler_spec.rb
@@ -232,7 +232,7 @@ RSpec.describe Rubysmith::Builders::Bundler, :realm do
         <<~CONTENT
           source "https://rubygems.org"
 
-          gem "refinements", "~> 7.18"
+          gem "refinements", "~> 8.0"
 
           group :development do
             gem "rake", "~> 13.0"
@@ -403,7 +403,7 @@ RSpec.describe Rubysmith::Builders::Bundler, :realm do
         <<~CONTENT
           source "https://rubygems.org"
 
-          gem "refinements", "~> 7.18"
+          gem "refinements", "~> 8.0"
 
           group :code_quality do
             gem "bundler-audit", "~> 0.7"
