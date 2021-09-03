6bfe32fd51e9e2a3faa2ae3447d36f522a0021e5
Brooke Kuhlmann <brooke@alchemists.io>
2021-09-02 20:03:22 -0600

    Added Zeitwerk configuration
    Now that the Zeitwerk gem has been added (see previous commit), we can
    use it to manage our dependencies implicitly.

--- lib/rubysmith.rb
+++ lib/rubysmith.rb
@@ -1,33 +1,11 @@
 # frozen_string_literal: true
 
-require "rubysmith/identity"
-require "rubysmith/pathway"
-require "rubysmith/renderers/namespace"
-require "rubysmith/renderers/erb"
-require "rubysmith/text/inserter"
-require "rubysmith/builder"
-require "rubysmith/builders/core"
-require "rubysmith/builders/documentation"
-require "rubysmith/builders/git/setup"
-require "rubysmith/builders/git/commit"
-require "rubysmith/builders/bundler"
-require "rubysmith/builders/rake"
-require "rubysmith/builders/console"
-require "rubysmith/builders/setup"
-require "rubysmith/builders/guard"
-require "rubysmith/builders/reek"
-require "rubysmith/builders/rspec/context"
-require "rubysmith/builders/rspec/helper"
-require "rubysmith/builders/pragma"
-require "rubysmith/builders/rubocop/setup"
-require "rubysmith/builders/rubocop/formatter"
-require "rubysmith/builders/ruby_critic"
-require "rubysmith/cli/configuration/content"
-require "rubysmith/cli/configuration/loader"
-require "rubysmith/cli/parsers"
-require "rubysmith/cli/parsers/core"
-require "rubysmith/cli/parsers/build"
-require "rubysmith/cli/parsers/assembler"
-require "rubysmith/cli/processors/config"
-require "rubysmith/cli/processors/build"
-require "rubysmith/cli/shell"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect "cli" => "CLI", "erb" => "ERB", "rspec" => "RSpec"
+loader.setup
+
+# Main namespace.
+module Rubysmith
+end
