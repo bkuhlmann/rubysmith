89b553c65b86c56b845c138ad24870d990a4c3c7
Brooke Kuhlmann <brooke@alchemists.io>
2021-09-18 17:47:55 -0600

    Added CLI version configuration enhancer
    Necessary to add this gem's current version to the configuration
    content when loaded. This will be used to display version information
    when the CLI version option is enabled. This will also reduce the hard
    coding of version information within the core parser.

---
+++ spec/lib/rubysmith/cli/configuration/enhancers/version_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Rubysmith::CLI::Configuration::Enhancers::Version do
+  subject(:enhancer) { described_class.new version }
+
+  let(:version) { "1.2.3" }
+  let(:content) { Rubysmith::CLI::Configuration::Content.new }
+
+  describe "#call" do
+    it "answers current time" do
+      expect(enhancer.call(content)).to have_attributes(version: "1.2.3")
+    end
+  end
+end
