a0874d3aba6889213c3e631e44ee010bf9813bb1
Brooke Kuhlmann <brooke@alchemists.io>
2021-09-18 18:58:19 -0600

    Removed RSpec CLI version matcher
    No longer needed now that this information is checked in the shell
    specs only.

--- spec/support/matchers/cli_version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec::Matchers.define :match_cli_version do
-  expected = /Rubysmith\s\d+\.\d+\.\d+/
-
-  match { |actual| actual.match? expected }
-
-  description { "match /#{expected.source}/" }
-end
