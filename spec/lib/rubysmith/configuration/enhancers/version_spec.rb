1767751e5706ca7814feb614aeadebff5bf80fd2
Brooke Kuhlmann <brooke@alchemists.io>
2021-11-23 13:46:39 -0700

    Refactored version configuration enhancer
    Moved content assignment within `#call` since it's specific to `#call`.

--- spec/lib/rubysmith/configuration/enhancers/version_spec.rb
+++ spec/lib/rubysmith/configuration/enhancers/version_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe Rubysmith::Configuration::Enhancers::Version do
   subject(:enhancer) { described_class.new version }
 
   let(:version) { "1.2.3" }
-  let(:content) { Rubysmith::Configuration::Content.new }
 
   describe "#call" do
-    it "answers current time" do
+    let(:content) { Rubysmith::Configuration::Content.new }
+
+    it "answers version" do
       expect(enhancer.call(content)).to have_attributes(version: "1.2.3")
     end
   end
