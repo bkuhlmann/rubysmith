e9c21ee377c537c3052adaa4a67630f759ba9340
Brooke Kuhlmann <brooke@alchemists.io>
2021-11-23 13:49:09 -0700

    Refactored current time configuration enhancer
    Moved content assignment within `#call` since it's specific to `#call`.

--- spec/lib/rubysmith/configuration/enhancers/current_time_spec.rb
+++ spec/lib/rubysmith/configuration/enhancers/current_time_spec.rb
@@ -6,9 +6,10 @@ RSpec.describe Rubysmith::Configuration::Enhancers::CurrentTime do
   subject(:enhancer) { described_class.new now }
 
   let(:now) { Time.now }
-  let(:content) { Rubysmith::Configuration::Content.new }
 
   describe "#call" do
+    let(:content) { Rubysmith::Configuration::Content.new }
+
     it "answers current time" do
       expect(enhancer.call(content)).to have_attributes(now: now)
     end
