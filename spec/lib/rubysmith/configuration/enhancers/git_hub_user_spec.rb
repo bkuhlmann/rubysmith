b73a69624d65945307e563b376e8a1d1076cd04f
Brooke Kuhlmann <brooke@alchemists.io>
2021-11-23 13:37:45 -0700

    Refactored GitHub user configuration enhancer specs
    Necessary to be consistent with the recently added Git enhancer in
    addition to cleaning up the spec descriptions to be more clear.

--- spec/lib/rubysmith/configuration/enhancers/git_hub_user_spec.rb
+++ spec/lib/rubysmith/configuration/enhancers/git_hub_user_spec.rb
@@ -5,49 +5,52 @@ require "spec_helper"
 RSpec.describe Rubysmith::Configuration::Enhancers::GitHubUser do
   subject(:enhancer) { described_class.new repository: repository }
 
-  let(:content) { Rubysmith::Configuration::Content[git_hub_user: "manual"] }
-  let(:repository) { instance_double GitPlus::Repository, config_get: user }
+  let(:repository) { instance_double GitPlus::Repository }
 
   describe "#call" do
-    context "with nil content and GitHub users" do
+    let(:content) { Rubysmith::Configuration::Content[git_hub_user: "default"] }
+
+    before { allow(repository).to receive(:config_get).with("github.user").and_return(user) }
+
+    context "with missing defaults and GitHub user" do
       let(:user) { nil }
+      let(:content) { Rubysmith::Configuration::Content.new }
 
       it "answers nil user" do
-        content = Rubysmith::Configuration::Content.new
         expect(enhancer.call(content)).to have_attributes(git_hub_user: nil)
       end
     end
 
-    context "with exiting content user and nil GitHub user" do
+    context "with missing defaults and existing GitHub user" do
+      let(:user) { "git_hub" }
+      let(:content) { Rubysmith::Configuration::Content.new }
+
+      it "answers GitHub user" do
+        expect(enhancer.call(content)).to have_attributes(git_hub_user: "git_hub")
+      end
+    end
+
+    context "with defaults and nil GitHub user" do
       let(:user) { nil }
 
-      it "answers manually defined user" do
-        expect(enhancer.call(content)).to have_attributes(git_hub_user: "manual")
+      it "answers default user" do
+        expect(enhancer.call(content)).to have_attributes(git_hub_user: "default")
       end
     end
 
-    context "with exiting content user and blank GitHub user" do
+    context "with defaults and blank GitHub user" do
       let(:user) { "" }
 
-      it "answers manually defined user" do
-        expect(enhancer.call(content)).to have_attributes(git_hub_user: "manual")
+      it "answers default user" do
+        expect(enhancer.call(content)).to have_attributes(git_hub_user: "default")
       end
     end
 
-    context "with exiting content and GitHub users" do
+    context "with defaults and GitHub user" do
       let(:user) { "dynamic" }
 
-      it "answers nil user" do
-        expect(enhancer.call(content)).to have_attributes(git_hub_user: "manual")
-      end
-    end
-
-    context "with nil content user and existing GitHub user" do
-      let(:user) { "dynamic" }
-
-      it "answers nil user" do
-        content = Rubysmith::Configuration::Content[git_hub_user: "dynamic"]
-        expect(enhancer.call(content)).to have_attributes(git_hub_user: "dynamic")
+      it "answers default user" do
+        expect(enhancer.call(content)).to have_attributes(git_hub_user: "default")
       end
     end
   end
