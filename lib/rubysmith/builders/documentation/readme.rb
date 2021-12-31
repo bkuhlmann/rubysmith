72ee693a3006073adcc17afc1b02403fb915953c
Brooke Kuhlmann <brooke@alchemists.io>
2021-12-31 15:02:51 -0700

    Fixed readme builder new line truncation
    Minor simplification to code since any new lines greater than two can
    be truncated.

--- lib/rubysmith/builders/documentation/readme.rb
+++ lib/rubysmith/builders/documentation/readme.rb
@@ -22,8 +22,7 @@ module Rubysmith
 
           builder.call(configuration.merge(template_path: "%project_name%/README.#{kind}.erb"))
                  .render
-                 .replace("\n\n\n", "\n\n")
-                 .replace("\n\n\n\n\n\n", "\n\n")
+                 .replace(/\n{2,}/, "\n\n")
 
           configuration
         end
