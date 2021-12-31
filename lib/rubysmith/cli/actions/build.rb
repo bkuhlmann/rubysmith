828a3111ec6b0627cd6e83dc94e7b2086447b419
Brooke Kuhlmann <brooke@alchemists.io>
2021-12-31 15:10:54 -0700

    Removed the contribution builder
    Necessary to reduce project maintenance since this documentation is the
    same for every project. It is better to fall back to the URL provided
    in the README.

--- lib/rubysmith/cli/actions/build.rb
+++ lib/rubysmith/cli/actions/build.rb
@@ -10,7 +10,6 @@ module Rubysmith
           Builders::Core,
           Builders::Documentation::Readme,
           Builders::Documentation::Citation,
-          Builders::Documentation::Contribution,
           Builders::Documentation::License,
           Builders::Documentation::Version,
           Builders::Git::Setup,
