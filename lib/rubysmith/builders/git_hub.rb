aaca3cbfa890516506e2f95f9b86508761f3e00c
Brooke Kuhlmann <brooke@alchemists.io>
2022-04-30 09:07:45 -0600

    Added GitHub builder funding configuration
    Necessary to build the funding configuration for a GitHub enabled
    project but only if the funding build option is used since not all
    GitHub projects might want funding support. Full CLI support -- as a
    build option -- will be added in the next commit.

--- lib/rubysmith/builders/git_hub.rb
+++ lib/rubysmith/builders/git_hub.rb
@@ -16,6 +16,8 @@ module Rubysmith
       end
 
       def call
+        render_funding
+
         return configuration unless configuration.build_git_hub
 
         builder.call(with_issue_template).render
@@ -26,6 +28,13 @@ module Rubysmith
       private
 
       attr_reader :configuration, :builder
+
+      def render_funding
+        return unless configuration.build_funding
+
+        configuration.merge(template_path: "%project_name%/.github/FUNDING.yml.erb")
+                     .then { |updated_configuration| builder.call(updated_configuration).render }
+      end
 
       def with_issue_template
         configuration.merge template_path: "%project_name%/.github/ISSUE_TEMPLATE.md.erb"
--- spec/lib/rubysmith/builders/git_hub_spec.rb
+++ spec/lib/rubysmith/builders/git_hub_spec.rb
@@ -9,14 +9,39 @@ RSpec.describe Rubysmith::Builders::GitHub do
 
   include_context "with application dependencies"
 
+  let(:funding_path) { temp_dir.join "test/.github/FUNDING.yml" }
   let(:issue_path) { temp_dir.join "test/.github/ISSUE_TEMPLATE.md" }
   let(:pull_request_path) { temp_dir.join "test/.github/PULL_REQUEST_TEMPLATE.md" }
 
   it_behaves_like "a builder"
 
   describe "#call" do
-    context "when enabled" do
+    context "when enabled with all options" do
+      let(:test_configuration) { configuration.maximize }
+
+      it "builds funding configuration" do
+        builder.call
+        expect(funding_path.exist?).to be(true)
+      end
+
+      it "builds issue template" do
+        builder.call
+        expect(issue_path.exist?).to be(true)
+      end
+
+      it "builds pull request template" do
+        builder.call
+        expect(pull_request_path.exist?).to be(true)
+      end
+    end
+
+    context "when enabled without funding" do
       let(:test_configuration) { configuration.minimize.merge build_git_hub: true }
+
+      it "does not build funding configuration" do
+        builder.call
+        expect(funding_path.exist?).to be(false)
+      end
 
       it "builds issue template" do
         builder.call
@@ -52,8 +77,32 @@ RSpec.describe Rubysmith::Builders::GitHub do
       end
     end
 
+    context "with funding enabled only" do
+      let(:test_configuration) { configuration.minimize.merge build_funding: true }
+
+      it "builds funding configuration" do
+        builder.call
+        expect(funding_path.read).to eq("github: [hubber]\n")
+      end
+
+      it "does not build issue template" do
+        builder.call
+        expect(issue_path.exist?).to be(false)
+      end
+
+      it "does not build pull request template" do
+        builder.call
+        expect(pull_request_path.exist?).to be(false)
+      end
+    end
+
     context "when disabled" do
       let(:test_configuration) { configuration.minimize }
+
+      it "does not build funding configuration" do
+        builder.call
+        expect(funding_path.exist?).to be(false)
+      end
 
       it "does not build issue template" do
         builder.call
