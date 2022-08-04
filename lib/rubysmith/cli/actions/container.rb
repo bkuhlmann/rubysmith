f29d4ae91b1eb0c29d88ab45e5995a7bbf43106c
Brooke Kuhlmann <brooke@alchemists.io>
2022-08-03 20:40:14 -0600

    Removed registration of duplicate keys within containers
    This behavior is unnecessary for merging containers and removes the
    possiblity of unnecessary mutation of registered objects within the
    container.

--- lib/rubysmith/cli/actions/container.rb
+++ lib/rubysmith/cli/actions/container.rb
@@ -9,8 +9,6 @@ module Rubysmith
       module Container
         extend Dry::Container::Mixin
 
-        config.registry = ->(container, key, value, _options) { container[key.to_s] = value }
-
         merge Rubysmith::Container
 
         register(:build) { Build.new }
