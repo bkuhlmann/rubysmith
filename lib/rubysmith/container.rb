28509400142428777e24581e148f08f39210abf7
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-06 11:19:02 -0600

    Updated container to use Etcher configuration
    Necessary to leverage Etcher and Runcom for loading and resolving the configuration.

--- lib/rubysmith/container.rb
+++ lib/rubysmith/container.rb
@@ -2,16 +2,34 @@
 
 require "cogger"
 require "dry-container"
+require "etcher"
+require "runcom"
 require "spek"
-require "tone"
 
 module Rubysmith
   # Provides a global gem container for injection into other objects.
   module Container
     extend Dry::Container::Mixin
 
-    register(:color) { Tone.new }
-    register(:configuration) { Configuration::Loader.call }
+    register :configuration do
+      self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
+                     .then { |registry| Etcher.call registry }
+    end
+
+    register :defaults do
+      Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
+                      .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
+                      .add_transformer(Configuration::Transformers::CurrentTime)
+                      .add_transformer(Configuration::Transformers::GitHubUser)
+                      .add_transformer(Configuration::Transformers::GitEmail)
+                      .add_transformer(Configuration::Transformers::GitUser)
+                      .add_transformer(Configuration::Transformers::TemplateRoot)
+                      .add_transformer(Configuration::Transformers::TargetRoot)
+    end
+
+    register(:input, memoize: true) { self[:configuration].dup }
+    register(:defaults_path) { Pathname(__dir__).join("configuration/defaults.yml") }
+    register(:xdg_config) { Runcom::Config.new "rubysmith/configuration.yml" }
     register(:specification) { Spek::Loader.call "#{__dir__}/../../rubysmith.gemspec" }
     register(:kernel) { Kernel }
     register(:logger) { Cogger.new formatter: :emoji }
--- spec/support/shared_contexts/application_dependencies.rb
+++ spec/support/shared_contexts/application_dependencies.rb
@@ -4,41 +4,43 @@ require "dry/container/stub"
 require "infusible/stub"
 
 RSpec.shared_context "with application dependencies" do
-  using Refinements::Structs
   using Infusible::Stub
 
   include_context "with temporary directory"
 
   let :configuration do
-    Rubysmith::Configuration::Loader.with_defaults.call.merge(
-      author_email: "jill@example.com",
-      author_family_name: "Smith",
-      author_given_name: "Jill",
-      author_url: "https://www.jillsmith.com",
-      citation_affiliation: "ACME",
-      citation_orcid: "0000-1111-2222-3333",
-      git_hub_user: "hubber",
-      now: Time.local(2020, 1, 1, 0, 0, 0),
-      project_name: "test",
-      project_url_community: "https://www.example.com/%project_name%/community",
-      project_url_conduct: "https://www.example.com/%project_name%/code_of_conduct",
-      project_url_contributions: "https://www.example.com/%project_name%/contributions",
-      project_url_download: "https://www.example.com/%project_name%/download",
-      project_url_home: "https://www.example.com/%project_name%",
-      project_url_issues: "https://www.example.com/%project_name%/issues",
-      project_url_license: "https://www.example.com/%project_name%/license",
-      project_url_security: "https://www.example.com/%project_name%/security",
-      project_url_source: "https://www.example.com/%project_name%/source",
-      project_url_versions: "https://www.example.com/%project_name%/versions",
-      target_root: temp_dir,
-      template_roots: [Bundler.root.join("lib/rubysmith/templates")]
-    )
+    Etcher.new(Rubysmith::Container[:defaults])
+          .call(
+            author_email: "jill@example.com",
+            author_family_name: "Smith",
+            author_given_name: "Jill",
+            author_url: "https://www.jillsmith.com",
+            citation_affiliation: "ACME",
+            citation_orcid: "0000-1111-2222-3333",
+            git_hub_user: "hubber",
+            now: Time.local(2020, 1, 1, 0, 0, 0),
+            project_name: "test",
+            project_url_community: "https://www.example.com/%project_name%/community",
+            project_url_conduct: "https://www.example.com/%project_name%/code_of_conduct",
+            project_url_contributions: "https://www.example.com/%project_name%/contributions",
+            project_url_download: "https://www.example.com/%project_name%/download",
+            project_url_home: "https://www.example.com/%project_name%",
+            project_url_issues: "https://www.example.com/%project_name%/issues",
+            project_url_license: "https://www.example.com/%project_name%/license",
+            project_url_security: "https://www.example.com/%project_name%/security",
+            project_url_source: "https://www.example.com/%project_name%/source",
+            project_url_versions: "https://www.example.com/%project_name%/versions",
+            target_root: temp_dir
+          )
+          .bind(&:dup)
   end
 
+  let(:input) { configuration.dup }
+  let(:xdg_config) { Runcom::Config.new Rubysmith::Container[:defaults_path] }
   let(:kernel) { class_spy Kernel }
   let(:logger) { Cogger.new io: StringIO.new, level: :debug, formatter: :emoji }
 
-  before { Rubysmith::Import.stub configuration:, kernel:, logger: }
+  before { Rubysmith::Import.stub configuration:, input:, xdg_config:, kernel:, logger: }
 
-  after { Rubysmith::Import.unstub :configuration, :kernel, :logger }
+  after { Rubysmith::Import.unstub :configuration, :input, :xdg_config, :kernel, :logger }
 end
