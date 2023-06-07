f418be78b9c17002f309168653e382a5743d7028
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-06 19:54:14 -0600

    Added CLI build command
    Necessary to handle the building of the project by leveraging the actions that were added earlier.

---
+++ lib/rubysmith/cli/commands/build.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require "refinements/structs"
+require "sod"
+
+module Rubysmith
+  module CLI
+    module Commands
+      # Stores table of contents root path.
+      class Build < Sod::Command
+        include Import[:inputs, :kernel, :logger]
+
+        # Order is important.
+        # rubocop:todo Metrics/CollectionLiteralLength
+        BUILDERS = [
+          Builders::Core,
+          Builders::Version,
+          Builders::Documentation::Readme,
+          Builders::Documentation::Citation,
+          Builders::Documentation::License,
+          Builders::Documentation::Version,
+          Builders::Git::Setup,
+          Builders::Git::Ignore,
+          Builders::Git::Safe,
+          Builders::Bundler,
+          Builders::Rake,
+          Builders::Console,
+          Builders::CircleCI,
+          Builders::Setup,
+          Builders::GitHub,
+          Builders::GitHubCI,
+          Builders::Guard,
+          Builders::Reek,
+          Builders::RSpec::Binstub,
+          Builders::RSpec::Context,
+          Builders::RSpec::Helper,
+          Builders::Caliber,
+          Extensions::Bundler,
+          Extensions::Pragmater,
+          Extensions::Tocer,
+          Extensions::Rubocop,
+          Builders::Git::Commit
+        ].freeze
+        # rubocop:enable Metrics/CollectionLiteralLength
+
+        handle "build"
+
+        description "Build new project."
+
+        on Actions::Name
+        on Actions::AmazingPrint
+        on Actions::Caliber
+        on Actions::Console
+        on Actions::Contributions
+        on Actions::CircleCI
+        on Actions::Citation
+        on Actions::Community
+        on Actions::Conduct
+        on Actions::Debug
+        on Actions::Funding
+        on Actions::Git
+        on Actions::GitHub
+        on Actions::GitHubCI
+        on Actions::GitLint
+        on Actions::Guard
+        on Actions::License
+        on Actions::Maximum
+        on Actions::Minimum
+        on Actions::Rake
+        on Actions::Readme
+        on Actions::Reek
+        on Actions::Refinements
+        on Actions::RSpec
+        on Actions::Security
+        on Actions::Setup
+        on Actions::SimpleCov
+        on Actions::Versions
+        on Actions::Yard
+        on Actions::Zeitwerk
+
+        def initialize(builders: BUILDERS, **)
+          super(**)
+          @builders = builders
+        end
+
+        def call
+          log_info "Building project skeleton: #{inputs.project_name}..."
+          builders.each { |builder| builder.call inputs }
+          log_info "Project skeleton complete!"
+        end
+
+        private
+
+        attr_reader :builders
+
+        def log_info(message) = logger.info { message }
+      end
+    end
+  end
+end
---
+++ spec/lib/rubysmith/cli/commands/build_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Rubysmith::CLI::Commands::Build do
+  subject(:command) { described_class.new builders: [builder] }
+
+  include_context "with application dependencies"
+
+  let(:builder) { class_spy Rubysmith::Builders::Core }
+
+  describe "#call" do
+    it "logs message" do
+      command.call
+      expect(logger.reread).to eq(<<~OUTPUT)
+        🟢 \e[32mBuilding project skeleton: test...\e[0m
+        🟢 \e[32mProject skeleton complete!\e[0m
+      OUTPUT
+    end
+
+    it "calls builders" do
+      command.call
+      expect(builder).to have_received(:call).with(configuration)
+    end
+  end
+end
