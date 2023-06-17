e9ca31c35b714530d32f67df02c36e268013fe89
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-17 14:06:24 -0600

    Updated Git user transformer to inject dependencies
    Necessary to allow upstream dependencies swap dependencies if desired.

--- lib/rubysmith/configuration/transformers/git_user.rb
+++ lib/rubysmith/configuration/transformers/git_user.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 require "dry/monads"
-require "gitt"
 
 module Rubysmith
   module Configuration
-    # Dynamically adds Git user if defined.
     module Transformers
-      include Dry::Monads[:result]
+      # Dynamically adds Git user if defined.
+      class GitUser
+        include Import[:git]
+        include Dry::Monads[:result]
 
-      GitUser = lambda do |content, git: Gitt::Repository.new|
-        if content[:author_given_name] || content[:author_family_name]
-          Dry::Monads::Success content
-        else
+        def call content
+          return Success content if content[:author_given_name] || content[:author_family_name]
+
           git.get("user.name").fmap do |name|
             first, last = String(name).split
             content.merge author_given_name: first, author_family_name: last
--- spec/lib/rubysmith/configuration/transformers/git_user_spec.rb
+++ spec/lib/rubysmith/configuration/transformers/git_user_spec.rb
@@ -5,30 +5,27 @@ require "spec_helper"
 RSpec.describe Rubysmith::Configuration::Transformers::GitUser do
   include Dry::Monads[:result]
 
-  subject(:transformer) { described_class }
+  subject(:transformer) { described_class.new git: }
+
+  let(:git) { instance_double Gitt::Repository }
 
   describe "#call" do
-    let :content do
-      {author_given_name: "Test", author_family_name: "Example"}
-    end
-
+    let(:content) { {author_given_name: "Test", author_family_name: "Example"} }
     let(:user) { Failure "Danger!" }
-    let(:git) { instance_double Gitt::Repository }
 
     before { allow(git).to receive(:get).with("user.name").and_return(user) }
 
     it "answers default given and family name when present" do
-      expect(transformer.call(content, git:)).to eq(
+      expect(transformer.call(content)).to eq(
         Success(author_given_name: "Test", author_family_name: "Example")
       )
     end
 
     context "without default given or family name and with GitHub user" do
-      let(:content) { {} }
       let(:user) { Success "Git Example" }
 
       it "answers Git user given and family name" do
-        expect(transformer.call(content, git:)).to eq(
+        expect(transformer.call({})).to eq(
           Success(
             author_given_name: "Git", author_family_name: "Example"
           )
@@ -36,28 +33,20 @@ RSpec.describe Rubysmith::Configuration::Transformers::GitUser do
       end
     end
 
-    context "with default given name only" do
-      let(:content) { {author_given_name: "Test"} }
-
-      it "answers given name" do
-        expect(transformer.call(content, git:)).to eq(Success(author_given_name: "Test"))
-      end
+    it "answers given name with default given name only" do
+      expect(transformer.call({author_given_name: "Test"})).to eq(
+        Success(author_given_name: "Test")
+      )
     end
 
-    context "with default family name only" do
-      let(:content) { {author_family_name: "Example"} }
-
-      it "answers family name" do
-        expect(transformer.call(content, git:)).to eq(Success(author_family_name: "Example"))
-      end
+    it "answers family name with default family name only" do
+      expect(transformer.call({author_family_name: "Example"})).to eq(
+        Success(author_family_name: "Example")
+      )
     end
 
-    context "without default given or family name and Git user" do
-      let(:content) { {} }
-
-      it "answers failure" do
-        expect(transformer.call(content, git:)).to eq(Failure("Danger!"))
-      end
+    it "answers failure without default given or family name and Git user" do
+      expect(transformer.call({})).to eq(Failure("Danger!"))
     end
   end
 end
