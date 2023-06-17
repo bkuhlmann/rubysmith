cf1414635a5f0aef2f1bbd48337a8d920799a1f3
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-17 14:13:15 -0600

    Fixed target root transformer spec descriptions
    Minor cleanup to make wording clear and consistent with other specs in this namespace.

--- spec/lib/rubysmith/configuration/transformers/target_root_spec.rb
+++ spec/lib/rubysmith/configuration/transformers/target_root_spec.rb
@@ -8,15 +8,15 @@ RSpec.describe Rubysmith::Configuration::Transformers::TargetRoot do
   subject(:transformer) { described_class }
 
   describe "#call" do
-    it "answers original path when key is present" do
+    it "answers content path when key is present" do
       expect(transformer.call({target_root: "a/path"})).to eq(Success(target_root: "a/path"))
     end
 
-    it "answers custom path with missing key but custom path" do
+    it "answers custom path with empty content" do
       expect(transformer.call({}, path: "a/path")).to eq(Success(target_root: "a/path"))
     end
 
-    it "answers current directory with missing key" do
+    it "answers current directory with empty content" do
       expect(transformer.call({})).to eq(Success(target_root: Pathname.pwd))
     end
   end
