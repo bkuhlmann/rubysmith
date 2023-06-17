7f24375a019341628e79cd06ba4c55418dbcf6f2
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-17 13:58:12 -0600

    Updated Git email transformer to inject dependencies
    Necessary to allow upstream dependencies swap dependencies if desired.

--- lib/rubysmith/configuration/transformers/git_email.rb
+++ lib/rubysmith/configuration/transformers/git_email.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 require "dry/monads"
-require "gitt"
 
 module Rubysmith
   module Configuration
-    # Dynamically adds Git email if defined.
     module Transformers
-      include Dry::Monads[:result]
+      # Dynamically adds Git email if defined.
+      class GitEmail
+        include Import[:git]
+        include Dry::Monads[:result]
 
-      GitEmail = lambda do |content, git: Gitt::Repository.new|
-        return Dry::Monads::Success content if content[:author_email]
+        def call content
+          return Success content if content[:author_email]
 
-        git.get("user.email").fmap { |email| content.merge! author_email: email }
+          git.get("user.email").fmap { |email| content.merge! author_email: email }
+        end
       end
     end
   end
--- spec/lib/rubysmith/configuration/transformers/git_email_spec.rb
+++ spec/lib/rubysmith/configuration/transformers/git_email_spec.rb
@@ -5,34 +5,31 @@ require "spec_helper"
 RSpec.describe Rubysmith::Configuration::Transformers::GitEmail do
   include Dry::Monads[:result]
 
-  subject(:transformer) { described_class }
+  subject(:transformer) { described_class.new git: }
+
+  let(:git) { instance_double Gitt::Repository }
 
   describe "#call" do
-    let(:content) { {author_email: "test@example.com"} }
     let(:email) { Failure "Danger!" }
-    let(:git) { instance_double Gitt::Repository }
 
     before { allow(git).to receive(:get).with("user.email").and_return(email) }
 
     it "answers default email when present" do
-      expect(transformer.call(content, git:)).to eq(Success(author_email: "test@example.com"))
+      expect(transformer.call({author_email: "test@example.com"})).to eq(
+        Success(author_email: "test@example.com")
+      )
     end
 
     context "without default email and with Git email" do
-      let(:content) { {} }
       let(:email) { Success "git@example.com" }
 
       it "answers Git email" do
-        expect(transformer.call(content, git:)).to eq(Success(author_email: "git@example.com"))
+        expect(transformer.call({})).to eq(Success(author_email: "git@example.com"))
       end
     end
 
-    context "without default email and Git email" do
-      let(:content) { {} }
-
-      it "answers failure" do
-        expect(transformer.call(content, git:)).to eq(Failure("Danger!"))
-      end
+    it "answers failure without default email and Git email" do
+      expect(transformer.call({})).to eq(Failure("Danger!"))
     end
   end
 end
