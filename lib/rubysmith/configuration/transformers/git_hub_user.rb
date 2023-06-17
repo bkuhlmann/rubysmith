49e54e5ca28ee505dbeded6ff9bd582932325499
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-17 14:01:26 -0600

    Updated GitHub user transformer to inject dependencies
    Necessary to allow upstream dependencies swap dependencies if desired.

--- lib/rubysmith/configuration/transformers/git_hub_user.rb
+++ lib/rubysmith/configuration/transformers/git_hub_user.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 require "dry/monads"
-require "gitt"
 
 module Rubysmith
   module Configuration
-    # Dynamically adds GitHub user if user is defined.
     module Transformers
-      include Dry::Monads[:result]
+      # Dynamically adds GitHub user if user is defined.
+      class GitHubUser
+        include Import[:git]
+        include Dry::Monads[:result]
 
-      GitHubUser = lambda do |content, git: Gitt::Repository.new|
-        return Dry::Monads::Success content if content[:git_hub_user]
+        def call content
+          return Success content if content[:git_hub_user]
 
-        git.get("github.user").fmap { |user| content.merge! git_hub_user: user }
+          git.get("github.user").fmap { |user| content.merge! git_hub_user: user }
+        end
       end
     end
   end
--- spec/lib/rubysmith/configuration/transformers/git_hub_user_spec.rb
+++ spec/lib/rubysmith/configuration/transformers/git_hub_user_spec.rb
@@ -5,34 +5,29 @@ require "spec_helper"
 RSpec.describe Rubysmith::Configuration::Transformers::GitHubUser do
   include Dry::Monads[:result]
 
-  subject(:transformer) { described_class }
+  subject(:transformer) { described_class.new git: }
+
+  let(:git) { instance_double Gitt::Repository }
 
   describe "#call" do
-    let(:content) { {git_hub_user: "default"} }
     let(:user) { Failure "Danger!" }
-    let(:git) { instance_double Gitt::Repository }
 
     before { allow(git).to receive(:get).with("github.user").and_return(user) }
 
     it "answers default user when present" do
-      expect(transformer.call(content, git:)).to eq(Success(git_hub_user: "default"))
+      expect(transformer.call({git_hub_user: "default"})).to eq(Success(git_hub_user: "default"))
     end
 
     context "without default user and with GitHub user" do
-      let(:content) { {} }
       let(:user) { Success "git_hub" }
 
       it "answers GitHub user" do
-        expect(transformer.call(content, git:)).to eq(Success(git_hub_user: "git_hub"))
+        expect(transformer.call({})).to eq(Success(git_hub_user: "git_hub"))
       end
     end
 
-    context "without default user and GitHub user" do
-      let(:content) { {} }
-
-      it "answers failure" do
-        expect(transformer.call(content, git:)).to eq(Failure("Danger!"))
-      end
+    it "answers failure without default user and GitHub user" do
+      expect(transformer.call({})).to eq(Failure("Danger!"))
     end
   end
 end
