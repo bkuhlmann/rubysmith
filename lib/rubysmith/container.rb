c78b0d3be05877149849ec64322623fab6686310
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-23 14:28:32 -0600

    Added container memoization
    Necessary to improve CLI boot and test suite performance by reducing unnecessary reloading of XDG configuration and related objects.

--- lib/rubysmith/container.rb
+++ lib/rubysmith/container.rb
@@ -12,12 +12,12 @@ module Rubysmith
   module Container
     extend Dry::Container::Mixin
 
-    register :configuration do
+    register :configuration, memoize: true do
       self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
                      .then { |registry| Etcher.call registry }
     end
 
-    register :defaults do
+    register :defaults, memoize: true do
       Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
                       .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
                       .add_transformer(Configuration::Transformers::CurrentTime)
@@ -28,12 +28,15 @@ module Rubysmith
                       .add_transformer(Configuration::Transformers::TargetRoot)
     end
 
+    register :specification, memoize: true do
+      Spek::Loader.call "#{__dir__}/../../rubysmith.gemspec"
+    end
+
     register(:input, memoize: true) { self[:configuration].dup }
-    register(:defaults_path) { Pathname(__dir__).join("configuration/defaults.yml") }
-    register(:xdg_config) { Runcom::Config.new "rubysmith/configuration.yml" }
-    register(:specification) { Spek::Loader.call "#{__dir__}/../../rubysmith.gemspec" }
-    register(:git) { Gitt::Repository.new }
-    register(:kernel) { Kernel }
-    register(:logger) { Cogger.new formatter: :emoji }
+    register(:defaults_path, memoize: true) { Pathname(__dir__).join("configuration/defaults.yml") }
+    register(:xdg_config, memoize: true) { Runcom::Config.new "rubysmith/configuration.yml" }
+    register(:git, memoize: true) { Gitt::Repository.new }
+    register(:logger, memoize: true) { Cogger.new formatter: :emoji }
+    register :kernel, Kernel
   end
 end
