5d24285ea844abeef1b47564abdcfb6ee48a9c6b
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-29 10:53:34 -0600

    Added additional configuration template root transformer specs
    Necessary to improve the specification and document full usage since this transformer is intended to always prepend new defaults each time it is called which allows upstream dependencies to customize as desired.

--- spec/lib/rubysmith/configuration/transformers/template_root_spec.rb
+++ spec/lib/rubysmith/configuration/transformers/template_root_spec.rb
@@ -8,11 +8,25 @@ RSpec.describe Rubysmith::Configuration::Transformers::TemplateRoot do
   subject(:transformer) { described_class.new }
 
   describe "#call" do
-    let(:content) { {template_roots: [Pathname.pwd]} }
+    let(:content) { {template_roots: Pathname.pwd} }
 
-    it "answers array with custom content appended to defaults" do
+    it "answers array with content path appended to default path" do
       expect(transformer.call(content)).to eq(
         Success(template_roots: [Bundler.root.join("lib/rubysmith/templates"), Pathname.pwd])
+      )
+    end
+
+    it "answers array with content paths appended to default paths" do
+      content = {template_roots: [Pathname("/one"), Pathname("/two")]}
+
+      expect(transformer.call(content)).to eq(
+        Success(
+          template_roots: [
+            Bundler.root.join("lib/rubysmith/templates"),
+            Pathname("/one"),
+            Pathname("/two")
+          ]
+        )
       )
     end
 
@@ -24,6 +38,21 @@ RSpec.describe Rubysmith::Configuration::Transformers::TemplateRoot do
     it "answers empty array without defaults or custom content" do
       transformer = described_class.new default: nil
       expect(transformer.call({})).to eq(Success({template_roots: []}))
+    end
+
+    it "prepends new default to existing default path" do
+      result = described_class.new.call({}).bind do |content|
+        described_class.new(default: Pathname("/test")).call content
+      end
+
+      expect(result).to eq(
+        Success(
+          template_roots: [
+            Pathname("/test"),
+            Bundler.root.join("lib/rubysmith/templates")
+          ]
+        )
+      )
     end
   end
 end
