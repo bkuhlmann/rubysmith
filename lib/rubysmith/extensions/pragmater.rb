1800052b999a374c36c45a39850cd3085a81cff0
Brooke Kuhlmann <brooke@alchemists.io>
2024-02-15 19:19:25 -0700

    Fixed Pragmater extension configuration
    This was an unfortunate oversight where removing the original embedded configuration associated with this gem fell back to Pragmater's default configuration. We need to have a specialized configuration for this gem instead which this now addresses properly. This includes ensuring Pragamater is run within the newly generated project skeleton root directory.
    
    Milestone: patch

--- lib/rubysmith/extensions/pragmater.rb
+++ lib/rubysmith/extensions/pragmater.rb
@@ -1,19 +1,32 @@
 # frozen_string_literal: true
 
 require "pragmater"
+require "refinements/pathname"
 
 module Rubysmith
   module Extensions
     # Ensures project skeleton has pragmas.
     class Pragmater
+      using Refinements::Pathname
+
       def self.call(...) = new(...).call
+
+      def self.custom_configuration
+        ::Pragmater::Configuration::Model[
+          comments: ["# frozen_string_literal: true"],
+          patterns: %w[**/*.rake **/*.rb *.gemspec exe/* bin/* config.ru *file]
+        ]
+      end
 
       def initialize configuration, client: ::Pragmater::Inserter.new
         @configuration = configuration
         @client = client
       end
 
-      def call = client.call && configuration
+      def call
+        configuration.project_root.change_dir { client.call self.class.custom_configuration }
+        configuration
+      end
 
       private
 
--- spec/lib/rubysmith/extensions/pragmater_spec.rb
+++ spec/lib/rubysmith/extensions/pragmater_spec.rb
@@ -3,11 +3,15 @@
 require "spec_helper"
 
 RSpec.describe Rubysmith::Extensions::Pragmater do
+  using Refinements::Pathname
+
   subject(:extension) { described_class.new configuration, client: }
 
   include_context "with application dependencies"
 
   let(:client) { instance_spy Pragmater::Inserter }
+
+  before { temp_dir.join("test/Gemfile").make_ancestors.touch }
 
   describe ".call" do
     it "answers configuration" do
@@ -19,7 +23,17 @@ RSpec.describe Rubysmith::Extensions::Pragmater do
     before { extension.call }
 
     it "delegates to client" do
-      expect(client).to have_received(:call)
+      expect(client).to have_received(:call).with(
+        Pragmater::Configuration::Model[
+          comments: ["# frozen_string_literal: true"],
+          patterns: %w[**/*.rake **/*.rb *.gemspec exe/* bin/* config.ru *file]
+        ]
+      )
+    end
+
+    it "adds frozen string literal" do
+      described_class.new(configuration).call
+      expect(temp_dir.join("test/Gemfile").read).to eq("# frozen_string_literal: true\n")
     end
 
     it "answers configuration" do
