072f072f7212491783d1985c86d7f8edc0d6bd2d
Brooke Kuhlmann <brooke@alchemists.io>
2024-03-24 07:32:33 -0600

    Updated setup script as a Ruby script
    Necessary to reduce the number of languages used in the project and stick with the core language, Ruby, for improved consistency.
    
    Milestone: patch

--- spec/lib/rubysmith/builders/setup_spec.rb
+++ spec/lib/rubysmith/builders/setup_spec.rb
@@ -20,15 +20,54 @@ RSpec.describe Rubysmith::Builders::Setup do
       it "builds setup script without Pry support" do
         builder.call
 
-        expect(build_path.read).to eq(<<~CONTENT)
-          #! /usr/bin/env bash
+        expect(build_path.read).to eq(<<~'CONTENT')
+          #! /usr/bin/env ruby
 
-          set -o nounset
-          set -o errexit
-          set -o pipefail
-          IFS=$'\\n\\t'
+          require "fileutils"
+          require "pathname"
 
-          bundle install
+          APP_ROOT = Pathname(__dir__).join("..").expand_path
+
+          Runner = lambda do |*arguments, kernel: Kernel|
+            kernel.system(*arguments) || kernel.abort("\nERROR: Command #{arguments.inspect} failed.")
+          end
+
+          FileUtils.chdir APP_ROOT do
+            puts "Installing dependencies..."
+            Runner.call "bundle install"
+          end
+        CONTENT
+      end
+
+      it "updates script permissions" do
+        builder.call
+        expect(build_path.stat.mode).to eq(33261)
+      end
+    end
+
+    context "when enabled with debug" do
+      let(:test_configuration) { configuration.minimize.merge build_setup: true, build_debug: true }
+
+      it "builds setup script without Pry support" do
+        builder.call
+
+        expect(build_path.read).to eq(<<~'CONTENT')
+          #! /usr/bin/env ruby
+
+          require "debug"
+          require "fileutils"
+          require "pathname"
+
+          APP_ROOT = Pathname(__dir__).join("..").expand_path
+
+          Runner = lambda do |*arguments, kernel: Kernel|
+            kernel.system(*arguments) || kernel.abort("\nERROR: Command #{arguments.inspect} failed.")
+          end
+
+          FileUtils.chdir APP_ROOT do
+            puts "Installing dependencies..."
+            Runner.call "bundle install"
+          end
         CONTENT
       end
 
