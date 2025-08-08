e18dcb477820d5d788e76f1a8d67f86829401b20
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-08 11:55:34 -0600

    Updated GitHub template builder to use issue configuration
    Necessary to ensure the issue template configuration is built and configured properly to ensure the GitHub UI picks up these changes. This includes updating the RSpec BOM to ensure these files are accounted for.
    
    Milestone: minor

--- lib/rubysmith/builders/git_hub/template.rb
+++ lib/rubysmith/builders/git_hub/template.rb
@@ -13,18 +13,23 @@ module Rubysmith
           return false unless settings.build_git_hub
 
           builder.call(with_issue).render
-          builder.call(with_code_review).render
+          builder.call(with_pull_request).render
+          builder.call(with_configuration).render
           true
         end
 
         private
 
         def with_issue
-          settings.merge template_path: "%project_name%/.github/ISSUE_TEMPLATE.md.erb"
+          settings.merge template_path: "%project_name%/.github/ISSUE_TEMPLATE/issue.md.erb"
         end
 
-        def with_code_review
+        def with_pull_request
           settings.merge template_path: "%project_name%/.github/PULL_REQUEST_TEMPLATE.md.erb"
+        end
+
+        def with_configuration
+          settings.merge template_path: "%project_name%/.github/ISSUE_TEMPLATE/config.yml.erb"
         end
       end
     end
--- spec/lib/rubysmith/builders/git_hub/template_spec.rb
+++ spec/lib/rubysmith/builders/git_hub/template_spec.rb
@@ -10,8 +10,9 @@ RSpec.describe Rubysmith::Builders::GitHub::Template do
   include_context "with application dependencies"
 
   describe "#call" do
-    let(:issue_path) { temp_dir.join "test/.github/ISSUE_TEMPLATE.md" }
+    let(:issue_path) { temp_dir.join "test/.github/ISSUE_TEMPLATE/issue.md" }
     let(:pull_request_path) { temp_dir.join "test/.github/PULL_REQUEST_TEMPLATE.md" }
+    let(:configuration_path) { temp_dir.join "test/.github/ISSUE_TEMPLATE/config.yml" }
 
     context "when enabled with all options" do
       before do
@@ -23,8 +24,21 @@ RSpec.describe Rubysmith::Builders::GitHub::Template do
         expect(issue_path.exist?).to be(true)
       end
 
-      it "builds code review template" do
+      it "builds pull request template" do
         expect(pull_request_path.exist?).to be(true)
+      end
+
+      it "builds issue configuration" do
+        expect(configuration_path.read).to eq(<<~CONTENT)
+          blank_issues_enabled: false
+          contact_links:
+            - name: Community
+              url: https://undefined.io/community
+              about: Please ask questions or discuss specifics here.
+            - name: Security
+              url: https://undefined.io/policies/security
+              about: Please report security vulnerabilities here.
+        CONTENT
       end
 
       it "answers true" do
@@ -42,7 +56,7 @@ RSpec.describe Rubysmith::Builders::GitHub::Template do
         expect(issue_path.exist?).to be(false)
       end
 
-      it "does not build code review template" do
+      it "does not build pull request template" do
         expect(pull_request_path.exist?).to be(false)
       end
 
