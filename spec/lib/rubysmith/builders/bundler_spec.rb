8ee0021858760444bad03f56565f7a79653c0ce6
Brooke Kuhlmann <brooke@alchemists.io>
2026-07-16 16:39:48 -0600

    Updated to SimpleCov 1.0.0
    link:https://github.com/simplecov-ruby/simplecov/blob/main/CHANGELOG.md#100-2026-07-12[Details].
    
    Milestone: major

--- spec/lib/rubysmith/builders/bundler_spec.rb
+++ spec/lib/rubysmith/builders/bundler_spec.rb
@@ -230,7 +230,7 @@ RSpec.describe Rubysmith::Builders::Bundler do
 
 
         group :quality do
-          gem "simplecov", "~> 0.22", require: false
+          gem "simplecov", "~> 1.0", require: false
         end
       CONTENT
     end
@@ -280,7 +280,7 @@ RSpec.describe Rubysmith::Builders::Bundler do
             gem "caliber", "~> 0.94"
             gem "git-lint", "~> 11.0"
             gem "reek", "~> 6.5", require: false
-            gem "simplecov", "~> 0.22", require: false
+            gem "simplecov", "~> 1.0", require: false
           end
 
           group :development do
@@ -324,7 +324,7 @@ RSpec.describe Rubysmith::Builders::Bundler do
             gem "caliber", "~> 0.94"
             gem "git-lint", "~> 11.0"
             gem "reek", "~> 6.5", require: false
-            gem "simplecov", "~> 0.22", require: false
+            gem "simplecov", "~> 1.0", require: false
           end
 
           group :development do
--- spec/lib/rubysmith/builders/rspec/helper_spec.rb
+++ spec/lib/rubysmith/builders/rspec/helper_spec.rb
@@ -226,15 +226,7 @@ RSpec.describe Rubysmith::Builders::RSpec::Helper do
         <<~BODY
           require "simplecov"
 
-          unless ENV["COVERAGE"] == "no"
-            SimpleCov.start do
-              add_filter %r(^/spec/)
-              enable_coverage :branch
-              enable_coverage_for_eval
-              minimum_coverage_by_file line: 95, branch: 95
-            end
-          end
-
+          SimpleCov.start :strict unless ENV["COVERAGE"] == "no"
           Bundler.require :tools
 
           require "test"
@@ -287,15 +279,7 @@ RSpec.describe Rubysmith::Builders::RSpec::Helper do
         <<~BODY
           require "simplecov"
 
-          unless ENV["COVERAGE"] == "no"
-            SimpleCov.start do
-              add_filter %r(^/spec/)
-              enable_coverage :branch
-              enable_coverage_for_eval
-              minimum_coverage_by_file line: 95, branch: 95
-            end
-          end
-
+          SimpleCov.start :strict unless ENV["COVERAGE"] == "no"
           Bundler.require :tools
 
           require "test"
